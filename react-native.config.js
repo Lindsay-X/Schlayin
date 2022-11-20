f9e4d757e0045cf3d8a72f2fe7909cd62c640547
Lindsay-X <70488690+Lindsay-X@users.noreply.github.com>
2022-11-19 19:42:44 -0500

    Initial commit

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+ module.exports = {
+  project: {
+      ios:{},
+      android:{}
+  },
+  assets:['./assets/fonts/'],
+}
